09b533d10b37077e839c426fa08b1e1e6d01f5c3
abbymartin <abbyvmartin51@gmail.com>
2025-12-04 20:28:34 -0500

    fix github api url and callback

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,9 +1,15 @@
 export default function handler(req, res) {
   const appName = process.env.GITHUB_APP_NAME || 'wordsync';
+  const frontendUrl = process.env.FRONTEND_URL || 'https://wordsync.abbymartin.net';
+  const callbackUrl = `${frontendUrl}/api/auth/callback`;
 
-  // Redirect to GitHub App installation page
-  // User can select which repositories to grant access to
-  const githubAppInstallUrl = `https://github.com/apps/${appName}/installations/new`;
+  // new users: should show app installation page
+  // existing users: should show repository selection and redirects back
+  const params = new URLSearchParams({
+    redirect_uri: callbackUrl
+  });
+
+  const githubAppInstallUrl = `https://github.com/apps/${appName}/installations/select_target?${params}`;
 
   res.redirect(302, githubAppInstallUrl);
 }
